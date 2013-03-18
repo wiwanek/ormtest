7c39f332b65b5b7622962df4b63d88015aabd3db
Wojtek Iwanek <wojtek.iwanek@hotmail.com>
2013-03-18 21:49:20 +0100

    Added examples of db operations.

--- FluentNHTest/FluentNHTest/Program.cs
+++ FluentNHTest/FluentNHTest/Program.cs
@@ -1,6 +1,7 @@
 ﻿using FluentNHibernate.Cfg;
 using FluentNHibernate.Cfg.Db;
 using NHibernate;
+using NHibernate.Criterion;
 using NHibernate.Tool.hbm2ddl;
 using System;
 using System.Collections.Generic;
@@ -14,7 +15,47 @@ namespace FluentNHTest
     {
         static void Main(string[] args)
         {
-            Configure();
+            ISessionFactory sessionFactory = Configure();
+
+            using (ISession session = sessionFactory.OpenSession())
+            {
+                using (ITransaction transaction = session.BeginTransaction())
+                {
+                    Author a = new Author { AuthorId = Guid.NewGuid(), Name = "Brian Herbert" };
+                    session.Save(a);
+
+                    session.Save(new Book { BookId = Guid.NewGuid(), Author = a, Title = "Dune" });
+
+                    transaction.Commit();
+                }
+
+                IList<Book> books = session.CreateCriteria(typeof(Book)).List<Book>();
+                foreach (var book in books)
+                {
+                    Console.WriteLine(book.Title + " - " + book.Author.Name);
+                }
+
+                using (ITransaction transaction = session.BeginTransaction())
+                {
+                    Author frank = new Author { AuthorId = Guid.NewGuid(), Name = "Frank Herbert" };
+                    session.Save(frank);
+                    Book b = session.CreateCriteria(typeof(Book)).Add(Restrictions.Eq("Title", "Dune")).UniqueResult<Book>();
+                    if (b != null)
+                    {
+                        b.Author = frank;
+                    }
+                    session.Update(b);
+
+                    transaction.Commit();
+                }
+
+                books = session.CreateCriteria(typeof(Book)).List<Book>();
+                foreach (var book in books)
+                {
+                    Console.WriteLine(book.Title + " - " + book.Author.Name);
+                }
+
+            }
 
             Console.ReadKey();
         }
