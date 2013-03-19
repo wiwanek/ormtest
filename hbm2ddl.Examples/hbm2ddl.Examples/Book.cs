dd7b4ad7607b29216b6d4bdec6d9a0393a8c62f6
Wojtek Iwanek <wojtek.iwanek@hotmail.com>
2013-03-20 00:02:49 +0100

    Initial commit of hbm2ddl.Examples solution

---
+++ hbm2ddl.Examples/hbm2ddl.Examples/Book.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+
+namespace hbm2ddl.Examples
+{
+    public class Book
+    {
+        public virtual Guid BookId { get; set; }
+        public virtual string Title { get; set; }
+
+        public virtual Author Author { get; set; }
+    }
+}
---
+++ hbm2ddl.Examples/hbm2ddl.Examples/Program.cs
@@ -0,0 +1,52 @@
+﻿using NHibernate;
+using NHibernate.Cfg;
+using NHibernate.Criterion;
+using NHibernate.Tool.hbm2ddl;
+using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+
+namespace hbm2ddl.Examples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Configuration config = new Configuration();
+                config.Configure();
+                //config.AddAssembly(typeof(Author).Assembly);
+                config.AddClass(typeof(Author));
+                //config.AddFile("Mappings/Author.hbm.xml");
+                //config.AddResource("Author.hbm.xml", Assembly.GetExecutingAssembly());
+                //config.AddInputStream();
+
+                var schema = new SchemaExport(config);
+                schema.Create(true, true);
+
+                config.AddClass(typeof(Book));
+                try
+                {
+                    new SchemaValidator(config).Validate();
+                }
+                catch (HibernateException he)
+                {
+                    Console.Error.WriteLine("Validation failed... " + he.Message);
+                    Console.WriteLine("Updating schema...");
+                    new SchemaUpdate(config).Execute(true, true);
+                
+                }
+                new SchemaExport(config).Drop(true, true);
+
+                
+            }
+            catch (Exception e)
+            {
+                Console.Error.WriteLine("E!:" + e.ToString());
+            }
+            Console.ReadKey();
+        }
+    }
+}
