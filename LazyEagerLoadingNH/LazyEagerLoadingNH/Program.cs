b58cfae90887663effabe2fd01eb5714976476fb
Wojtek Iwanek <wojtek.iwanek@hotmail.com>
2013-06-05 21:57:39 +0200

    Replaced ICriteria with IQueryOver. Cleaned Program class.

--- LazyEagerLoadingNH/LazyEagerLoadingNH/Program.cs
+++ LazyEagerLoadingNH/LazyEagerLoadingNH/Program.cs
@@ -45,7 +45,7 @@ namespace FluentNHTest
                 // Lazy loading examples
                 using (ISession session = sessionFactory.OpenSession())
                 {
-                    IList<Book> books = session.CreateCriteria<Book>().List<Book>();
+                    IList<Book> books = session.QueryOver<Book>().List();// CreateCriteria<Book>().List<Book>();
                     foreach (Book b in books)
                     {
                         Console.Out.WriteLine(b.Title + " Author: " + b.Author.Name);
@@ -53,7 +53,7 @@ namespace FluentNHTest
                 }
                 using (ISession session = sessionFactory.OpenSession())
                 {
-                    IList<Author> authors = session.CreateCriteria<Author>().List<Author>();
+                    IList<Author> authors = session.QueryOver<Author>().List();// CreateCriteria<Author>().List<Author>();
                     foreach (Author a in authors)
                     {
                         Console.Out.WriteLine(a.Name + " books:");
@@ -66,7 +66,7 @@ namespace FluentNHTest
                 // Eager loading examples
                 using (ISession session = sessionFactory.OpenSession())
                 {
-                    IList<Book> books = session.CreateCriteria<Book>().SetFetchMode("Author",FetchMode.Eager).List<Book>();
+                    IList<Book> books = session.QueryOver<Book>().Fetch(b => b.Author).Eager.List();// CreateCriteria<Book>().SetFetchMode("Author", FetchMode.Eager).List<Book>();
                     
                     foreach (Book b in books)
                     {
@@ -75,7 +75,7 @@ namespace FluentNHTest
                 }
                 using (ISession session = sessionFactory.OpenSession())
                 {
-                    IList<Author> authors = session.CreateCriteria<Author>().SetFetchMode("Books", FetchMode.Eager).List<Author>();
+                    IList<Author> authors = session.QueryOver<Author>().Fetch(a => a.Books).Eager.List();// CreateCriteria<Author>().SetFetchMode("Books", FetchMode.Eager).List<Author>();
                     foreach (Author a in authors)
                     {
                         Console.Out.WriteLine(a.Name + " books:");
@@ -86,19 +86,6 @@ namespace FluentNHTest
                     }
                 }
                 // Fixed multiple author occurences
-                using (ISession session = sessionFactory.OpenSession())
-                {
-                    IList<Author> authors = session.CreateCriteria<Author>().SetFetchMode("Books", FetchMode.Eager).SetResultTransformer(new DistinctRootEntityResultTransformer()).List<Author>();
-                    foreach (Author a in authors)
-                    {
-                        Console.Out.WriteLine(a.Name + " books:");
-                        foreach (Book b in a.Books)
-                        {
-                            Console.Out.WriteLine("\t" + b.Title);
-                        }
-                    }
-                }
-                // using QueryOver
                 using (ISession session = sessionFactory.OpenSession())
                 {
                     IList<Author> authors = session.QueryOver<Author>().Fetch(a => a.Books).Eager.TransformUsing(Transformers.DistinctRootEntity).List();
